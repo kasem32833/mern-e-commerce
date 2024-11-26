f57c2004174f283c2b4cf4e59361c68ccff7f8a5
Kasem32833 <abulkasem32833@gmail.com>
2024-11-26 13:52:44 +0300

    initialize frontend and backend

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+// create a database connection
+
+mongoose.connect('mongodb+srv://admin:admin@e-commerce.bhkfh.mongodb.net/')
+.then(()=>console.log("Mongodb Connected"))
+.catch((error)=>console.log(error))
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.use(
+    cors({
+        origin: ' http://localhost:5173/' ,
+        methods: ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "Cash-Control",
+            "Expires",
+            "Pragma"
+        ],
+        credentials: true
+    })
+
+    
+)
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, ()=>console.log(`Server is running on port : ${PORT}`))
+
+
