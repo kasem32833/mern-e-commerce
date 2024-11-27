d65a53930bd6a36934175fad2ead64249c91b93b
Kasem32833 <abulkasem32833@gmail.com>
2024-11-27 11:51:54 +0300

    add store

---
+++ client/src/components/auth/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AuthLayout = () => {
+  return (
+    <div className='flex min-h-screen w-full'>
+        <div className="hidden lg:flex items-center justify-center">
+            
+        </div>
+    </div>
+  )
+}
+
+export default AuthLayout
